2bff9205d70450f93e9ca0d37de7eee763bededa
Ilyas Hamadouche <ilyas.match@gmail.com>
2015-10-15 22:41:38 +0100

    part1.java is modified

--- Part1/part1.java
+++ Part1/part1.java
@@ -4,8 +4,8 @@ public class part1{
 		
 		System.out.println("	JJJ     A     V     V     A	    \n");
 		System.out.println("	 J     A A     V   V     A A    \n");
-		System.out.println("	 J    AAAAA     V V	    AAAAA   \n");
-		System.out.println("	JJ   A	   A     V 	   A     A  \n");
+		System.out.println("	 J    AAAAA     V V     AAAAA   \n");
+		System.out.println("	JJ   A	   A     V     A     A  \n");
 		
 	}
 }
