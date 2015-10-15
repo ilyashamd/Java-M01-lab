fe296d13aca086602626d336cba657af9de7a04c
Ilyas Hamadouche <ilyas.match@gmail.com>
2015-10-15 22:43:23 +0100

    part1.java is modified

--- Part1/part1.java
+++ Part1/part1.java
@@ -2,7 +2,7 @@ public class part1{
 	
 	public static void main(String[] args){
 		
-		System.out.println("	JJJ     A     V     V     A	    \n");
+		System.out.println("	JJJ     A     V     V     A     \n");
 		System.out.println("	 J     A A     V   V     A A    \n");
 		System.out.println("	 J    AAAAA     V V     AAAAA   \n");
 		System.out.println("	JJ   A	   A     V     A     A  \n");
