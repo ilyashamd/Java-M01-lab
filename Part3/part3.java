f4cdfa891e429d5fd303ae0ff1994d71d503bea0
Ilyas Hamadouche <ilyas.match@gmail.com>
2015-10-15 22:33:29 +0100

    part3.java is added

---
+++ Part3/part3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class part3{
+	
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please, enter an integer number: ");
+		int i = input.nextInt();
+		
+		int d=i%2;
+		if(d==1) 	System.out.println("The number is an odd number\n");
+		else 		System.out.println("The number is an even number\n");
+		
+	}
+}
