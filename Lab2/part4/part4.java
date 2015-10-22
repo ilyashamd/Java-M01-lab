e42dd86bd217e686790a9660cee68233e3718f31
Ilyas Hamadouche <ilyas.match@gmail.com>
2015-10-22 11:13:13 +0100

    Part 2&3 are added

--- Lab2/part4/part4.java
+++ Lab2/part4/part4.java
@@ -1,17 +1,1 @@
-
-import java.util.Scanner;
-
-public class part4{
-	
-	public static void main(String[] args){
-		
-		Scanner input = new Scanner(System.in);
-		System.out.print("Please, enter an integer number: ");
-		int i = input.nextInt();
-		
-		int d=i%2;
-		if(d==1) 	System.out.println("The number is an odd number\n");
-		else 		System.out.println("The number is an even number\n");
-		
-	}
-}
+/* Not yet */
