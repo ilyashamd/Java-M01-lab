d8f9e5703173a3c7d9a8cb8bccb6a0b9abb2e908
Ilyas Hamadouche <ilyas.match@gmail.com>
2015-10-23 16:45:23 +0100

    part4 added

--- Lab2/part4/part4.java
+++ Lab2/part4/part4.java
@@ -1,1 +1,42 @@
-/* Not yet */
+/* Problem: Write a method which sorts in increasing order the elements of an integer array of length N */
+
+import java.util.Scanner;
+
+public class part4{
+	static Scanner input= new Scanner(System.in);
+	
+	public static void main(String[] args){
+		System.out.print("Pleas, Enter the length of the array: ");
+		int d = input.nextInt();
+		int []array= new int [d];
+		System.out.println("Enter the elements of the array: ");
+		for(int i=0; i<d; i++){
+			array[i]=input.nextInt();
+		}
+		sortArray(array, d);
+		showArray(array,d);	
+	}
+	
+	static void showArray(int []_array, int n){
+		System.out.print("[ ");
+		for(int i=0; i<n; i++){
+			System.out.print(_array[i]+"  ");
+		}
+		System.out.println("]");
+	}
+	
+	static void sortArray(int[]_array,int n){
+		boolean flag=false;
+		while(!flag){
+			flag=true;
+			for(int i=0; i<n-1; i++){
+				if(_array[i]>_array[i+1]){
+					flag=false;
+					int dummy=_array[i+1];
+					_array[i+1]=_array[i];
+					_array[i]=dummy;
+				}
+			}
+		}
+	}
+}
